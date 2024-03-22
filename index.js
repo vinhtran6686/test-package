28e3bedaf00fd8053d6fb7f253de31a0b0c818d7
vinhtran6686 <vinhtran.6686@gmail.com>
2024-03-22 13:10:14 +0700

    Add divide function to calculate division, update package version to 1.1.0

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ module.exports = {
          throw new Error('Both arguments must be numbers');
       }
       return num1 + num2;
+   },
+   divide: function(num1, num2) {
+      if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+         throw new Error('Both arguments must be numbers');
+      }
+      if (num2 === 0) {
+         throw new Error('Cannot divide by zero');
+      }
+      return num1 / num2;
    }
 };
